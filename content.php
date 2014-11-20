f81ba5b1cca6773b05962311d8b5cba413ca4aa4
hawkeye126 <pete.lower@gmail.com>
2014-11-20 13:20:05 -0800

    add content

--- content.php
+++ content.php
@@ -1,22 +1,7 @@
 <?php
 
 ?>
-		<div class="header-container">
-            <header class="wrapper clearfix">
-			<h1 class="title"><a href="#area-one">Pete Lower</a></h1>
-                <nav>
-                    <ul>
-                        <li><a href="#area-two">Dev</a></li>
-                        <li><a href="#area-three">Music</a></li>
-                        <li><a href="#area-four">Life</a></li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
-
-        <div class="main-container">
-            <div id="area-one" class="main wrapper clearfix area-one">
-
+			<!-- <div id="area-one" class="main wrapper clearfix area-one">
                 <article>
                     <header>
                         <h2>Slider Header</h2>
@@ -28,7 +13,7 @@
                     <footer>
                         <h3>Slider Nav</h3>
                     </footer>
-                </article>
+                </article> -->
 
                 <!-- <aside>
                     <h4>aside</h4>
@@ -40,53 +25,64 @@
 
                 <article>
                     <header>
-                        <h2>article header h1</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
+                        <h2>Internet Development and Programming</h2>
+                        <p>I've always been fascinated with how things work and took to computers at an early age.  Programming keeps me on my toes learning something new everyday!</p>
                     </header>
                     <section>
-                        <h2>article section h2</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+                        <h2>Languages</h2>
+                        <p>I am very well experience in the following languages.  Having worked on many websites and web applications I have dabbled in many many more.
+                        <div class='prog-lang-holder' style='text-align:center;vertical-align:top;'>
+                        	<br />
+                        	<img class='img-one css3-img' src='img/css3.svg' />
+                        	<img class='img-two html5-img' src='img/html5.svg' />
+                        	<img class='img-three javascript-img' src='img/javascript.svg' /><br/><br/>
+                        	<img class='img-four php-img' src='img/php.svg' />
+                        </div>
                     </section>
                     <section>
-                        <h2>article section h2</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+                        <h2>Experience</h2>
+                        <p>I started developing websites in high school (2006) with front page and pure HTML.  While attending the University of Iowa I worked on the University's web development team creating static, dynamic, and CMS based websites for different departments and organizations.  Since graduating in 2012 I created my own webdev shop, LowerMedia, which works with clients all over the United States to grow and strengthen their web presence.</p>
                     </section>
+                    <section>
+                        <h2>Please Checkout <br/><a href='http://lowermedia.net'>LowerMedia.Net<a/><br/> for Work Examples</h2>
+	                </section>
                     <footer>
-                        <h3>article footer h3</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
+                        <!-- <h3></h3>
+                        <p></p> -->
                     </footer>
                 </article>
 
-                <aside>
+                <!-- <aside>
                     <h4>aside</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
-                </aside>
+                </aside> -->
 
             </div> <!-- #main -->
             <div id="area-three" class="main wrapper clearfix area-three">
 
                 <article>
                     <header>
-                        <h2>article header h1</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
+                        <h2>Music Production, Songwriting, Guitar</h2>
+                        <p>I've been in and out of bands and writing songs since high school.  Over the years I have had the opportunity to tour around the United States playing shows and recording music.</p>
                     </header>
                     <section>
-                        <h2>article section h2</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+                        <h2>Item 9 & The Mad Hatters - Self Titled</h2>
+                        <h3>(Recorded Guitar, Vocals, Wrote and Produced)</h3>
+                        <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/34158255&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
                     </section>
                     <section>
-                        <h2>article section h2</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+                        <h2>Item 9 & The Mad Hatters - The Fall of Gondolin</h2>
+                        <iframe width="560" height="315" src="//www.youtube.com/embed/u9Aof9DYEUQ" frameborder="0" allowfullscreen></iframe>
                     </section>
                     <footer>
-                        <h3>article footer h3</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
+                        <!-- <h3></h3>
+                        <p></p> -->
                     </footer>
                 </article>
 
                 <aside>
-                    <h4>aside</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
+                    <h4>Item 9 & The Mad Hatters - They're Watchin' You</h4>
+                    <iframe width="287" height="161" src="//www.youtube.com/embed/Z5NtNKJMrTg" frameborder="0" allowfullscreen></iframe>
                 </aside>
 
             </div> <!-- #main -->
@@ -94,26 +90,25 @@
 
                 <article>
                     <header>
-                        <h2>article header h1</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
+                        <h2>Life in the Emerald City</h2>
+                        <p>I'm currently living it up in Seattle making music and working with local businesses to create dynamic web applications.</p>
                     </header>
                     <section>
-                        <h2>article section h2</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+                        <h2>Looking for a PHP, WordPress or Drupal Developer?</h2>
+                        <p>Contact me today: <br/>
+                        	<a href='mailto:pete@petelower.com'>pete@petelower.com</a><br/>
+                        	<a href='tel:+13195941786'>319 594 1786</a></p>
                     </section>
-                    <section>
-                        <h2>article section h2</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
-                    </section>
+                    
                     <footer>
-                        <h3>article footer h3</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
+                        <!-- <h3></h3>
+                        <p></p> -->
                     </footer>
                 </article>
 
                 <aside>
-                    <h4>aside</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
+                    <h4>Gas Works Park</h4>
+                    <p><img width='278' src='img/pete-kelsey-seattle.jpg' /></p>
                 </aside>
 
             </div> <!-- #main -->
