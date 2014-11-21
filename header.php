ea06ab7b8d9c65eea588b453b584f047c7dc5785
hawkeye126 <pete.lower@gmail.com>
2014-11-20 16:26:05 -0800

    organize

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
-        <?php 
+        <?php //validation fix
             if (isset($_SERVER['HTTP_USER_AGENT']) && 
                 (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false))
                     header('X-UA-Compatible: IE=edge,chrome=1');
@@ -16,17 +16,20 @@
         <meta name="description" content="I'm Pete Lower, I write code for the Internet (PHP, Javascript) and make music (i9mh.com).">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- STYLESHEETS -->
         <link rel="stylesheet" href="css/normalize.min.css">
         <!-- <link rel="stylesheet" href="css/main.css"> -->
         <link rel="stylesheet" href="css/style.css">
 
-        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+        <!-- FONTS -->
+        <link href='http://fonts.googleapis.com/css?family=Lobster|Changa+One|Merriweather' rel='stylesheet' type='text/css'>
+        
+        <!-- SCRIPTS -->
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 		<script src="js/vendor/jquery.sticky.js"></script>
 		<script src="js/plugins.js"></script>
 
-		<link href='http://fonts.googleapis.com/css?family=Lobster|Changa+One|Merriweather' rel='stylesheet' type='text/css'>
-        
     </head>
     <body>
         <!--[if lt IE 7]>
@@ -41,9 +44,9 @@
 			<h1 class="title"><a href="/">PeteLower.Com</a></h1>
                 <nav>
                     <ul>
-                        <li><a href="#area-two">Dev</a></li>
-                        <li><a href="#area-three">Music</a></li>
-                        <li><a href="#area-four">Life</a></li>
+                        <li><a href="#development">Dev</a></li>
+                        <li><a href="#music">Music</a></li>
+                        <li><a href="#life">Life</a></li>
                         <li><a href="http://peteygreen.wordpress.com">Blog</a></li>
                     </ul>
                 </nav>
