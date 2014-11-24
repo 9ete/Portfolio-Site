046ae31d6b8d2f987d8c5a0b5a69cfe6043c0b41
hawkeye126 <pete.lower@gmail.com>
2014-11-24 15:11:14 -0800

    removing unneded script files

--- footer.php
+++ footer.php
@@ -39,8 +39,8 @@
         <script src="js/vendor/jquery.sticky.js"></script>
         <script src="js/plugins.js"></script>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+        <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script> -->
 
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
