cfd29b8539ea86797b3a6940f43e3a9185bdadc5
hawkeye126 <pete.lower@gmail.com>
2014-11-24 11:58:33 -0800

    add google analytics

--- header.php
+++ header.php
@@ -15,6 +15,16 @@
         <title>Pete Lower | Musician and Programmer</title>
         <meta name="description" content="I'm Pete Lower, I write code for the Internet (PHP, Javascript) and make music (i9mh.com).">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-21432914-2', 'auto');
+          ga('send', 'pageview');
+
+        </script>
     </head>
     <body>
         <!--[if lt IE 7]>
