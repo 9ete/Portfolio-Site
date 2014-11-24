61560c9504f74ba94faca22445f1b0617fe8bc5c
hawkeye126 <pete.lower@gmail.com>
2014-11-24 15:24:10 -0800

    formatting content

--- content.php
+++ content.php
@@ -33,11 +33,6 @@
                     </footer>
                 </article>
 
-                <!-- <aside>
-                    <h4>aside</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
-                </aside> -->
-
             </div> <!-- #main -->
             <div id="music" class="main wrapper clearfix area-three music">
 
