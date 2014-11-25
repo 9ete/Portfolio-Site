234c606495d63a61353faca82e466a12c2105716
hawkeye126 <pete.lower@gmail.com>
2014-11-24 18:39:29 -0800

    add css and js to hide jssor slider until fully loaded

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+//HIDE JSSOR SLIDER UNTIL FULLY LOADED
+window.onload = function() {
+   //document.getElementById("slider1_container").style.visibility = "visible"; //Use visibility to keep a blank placeholder
+   document.getElementById("slider1_container").style.display = "block";
+};
