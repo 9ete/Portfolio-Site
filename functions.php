f2181d498f28794e6a483ca0c9a0c41770cb2b5f
hawkeye126 <pete.lower@gmail.com>
2014-11-26 11:52:58 -0800

    move GA to script function includes

--- functions.php
+++ functions.php
@@ -12,13 +12,25 @@ function is_mobile() {
 }
 
 function plScriptsStyles() { ?>
+    <!-- GOOGLE ANALYTICS -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-21432914-2', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+
     <!-- FONTS -->
     <link href='http://fonts.googleapis.com/css?family=Lobster|Changa+One|Merriweather' rel='stylesheet' type='text/css'>
 
     <!-- MINIFIED SCRIPTS -->
     <script type="application/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type="application/javascript" src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-    
+
     <?php if ($_SERVER['SERVER_NAME']=='dev.petelower.com') : ?>
 
         <!-- STYLESHEETS -->
