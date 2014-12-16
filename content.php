c13c8f50543e8a258c900a6f300474fab1fcdbb3
hawkeye126 <pete.lower@gmail.com>
2014-12-16 10:48:58 -0800

    remove invalid markup

--- content.php
+++ content.php
@@ -1,7 +1,7 @@
 <?php
 
 ?>
-            </div> <!-- #main -->
+            
 
             <div id="development" class="main wrapper clearfix area-two development">
 
@@ -15,10 +15,10 @@
                         <p>I am very well experience in the following languages.  Having worked on many websites and web applications I have dabbled in many many more.
                         <div class='prog-lang-holder' style='text-align:center;vertical-align:top;'>
                         	<br />
-                        	<img class='img-one css3-img' src='img/css3.svg' />
-                        	<img class='img-two html5-img' src='img/html5.svg' />
-                        	<img class='img-three javascript-img' src='img/javascript.svg' /><br/><br/>
-                        	<img class='img-four php-img' src='img/php.svg' />
+                        	<img class='img-one css3-img' src='img/css3.svg' alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
+                        	<img class='img-two html5-img' src='img/html5.svg' alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
+                        	<img class='img-three javascript-img' src='img/javascript.svg' alt="Seattle LAMP Developer and WordPress Expert Pete Lower" /><br/><br/>
+                        	<img class='img-four php-img' src='img/php.svg' alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
                         </div>
                     </section>
                     <section>
@@ -28,7 +28,7 @@
                             Since graduating in 2012 I started my own development shop, <strong>LowerMedia</strong>, which works with clients all over the United States to grow and strengthen their web presence.</p>
                     </section>
                     <section>
-                        <h2>Please Checkout <br/><a href='http://lowermedia.net'>LowerMedia.Net<a/><br/> for Work Examples</h2>
+                        <h2>Please Checkout <br /><a href='http://lowermedia.net'>LowerMedia.Net</a><br /> for Work Examples</h2>
 	                </section>
                     <footer>
                         <!-- <h3></h3>
@@ -36,7 +36,7 @@
                     </footer>
                 </article>
 
-            </div> <!-- #main -->
+            </div> <!-- #development -->
             <div id="music" class="main wrapper clearfix area-three music">
 
                 <article>
@@ -46,8 +46,14 @@
                     </header>
                     <section>
                         <h2>Item 9 & The Mad Hatters - Self Titled</h2>
-                        <h3><!-- (Recorded Guitar, Vocals, Wrote and Produced) --></h3>
-                        <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/34158255&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+                        <!--<h3> (Recorded Guitar, Vocals, Wrote and Produced) </h3>-->
+                         <!-- scrolling="no" frameborder="no" -->
+                        <iframe 
+                            width="515" 
+                            height="450" 
+                            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/34158255&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
+                            >
+                        </iframe>
                     </section>
                     
                     <footer>
@@ -57,14 +63,24 @@
                 </article>
 
                 <aside>
-                    <h4>Item 9 & The Mad Hatters - They're Watchin' You</h4>
-                    <iframe width="287" height="161" src="//www.youtube.com/embed/Z5NtNKJMrTg" frameborder="0" allowfullscreen></iframe>
+                    <h4>Item 9 & The Mad Hatters - They're Watchin' You</h4><!-- frameborder="0" -->
+                    <iframe 
+                        width="287" 
+                        height="161" 
+                        src="//www.youtube.com/embed/Z5NtNKJMrTg" 
+                        allowfullscreen>
+                    </iframe>
                     <br />
-                    <h4>Item 9 & The Mad Hatters - The Fall of Gondolin</h4>
-                    <iframe width="287" height="161" src="//www.youtube.com/embed/u9Aof9DYEUQ" frameborder="0" allowfullscreen></iframe>
+                    <h4>Item 9 & The Mad Hatters - The Fall of Gondolin</h4><!-- frameborder="0" -->
+                    <iframe 
+                        width="287"     
+                        height="161" 
+                        src="//www.youtube.com/embed/u9Aof9DYEUQ"  
+                        allowfullscreen>
+                    </iframe>
                 </aside>
 
-            </div> <!-- #main -->
+            </div> <!-- #music -->
             <div id="life" class="main wrapper clearfix area-four life">
 
                 <article>
@@ -88,8 +104,14 @@
 
                 <aside>
                     <h4>Gas Works Park</h4>
-                    <p><img width='278' src='img/seattle-tacoma-web-developer-wordpress-drupal-php-pete-lower.jpg' /></p>
+                    <p>
+                        <img 
+                            width='278' 
+                            src='img/seattle-tacoma-web-developer-wordpress-drupal-php-pete-lower.jpg' 
+                            alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
+                    </p>
                 </aside>
 
-            </div> <!-- #main -->
-    </div> <!-- #main-container -->
+            </div> <!-- #life -->
+
+        </div> <!-- #main-container -->
