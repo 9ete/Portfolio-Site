d3de19cf3282e11da74baa0e8d7bb0c74e057374
hawkeye126 <pete.lower@gmail.com>
2014-12-23 17:34:00 -0800

    move styles to stylesheet

--- slider.php
+++ slider.php
@@ -1,96 +1,100 @@
-    
-    <!-- Jssor Slider Begin -->
-    <!-- You can move inline styles to css file or css block. -->
-    <div id="slider1_container" style="position: relative; margin: 0 auto;
-        top: 0px; left: 0px; width: 1300px; height: 400px; overflow: hidden;">
-        <!-- Loading Screen -->
-        <!-- <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
-            <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block;
-                top: 0px; left: 0px; width: 100%; height: 100%;">
-            </div>
-            <div style="position: absolute; display: block; background: url(img/loading.gif) no-repeat center center;
-                top: 0px; left: 0px; width: 100%; height: 100%;">
-            </div>
-        </div> -->
+    <div id="slider1_container" class="slider1_container" style="">
         <!-- Slides Container -->
-        <div data-u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 1300px;
-            height: 400px; overflow: hidden;">
+        <div id="slides" class="slides" data-u="slides" style="">
             <div>
-                <!-- <img data-u="image" src="../slider/img/1920/red.jpg" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" /> -->
-                <img data-u="image" src="img/seattle-bridge-web-developer-pete-lower-wordpress-drupal-php.jpg" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                <!-- <div data-u="caption" data-t="NO" t3="RTT|2" r3="137.5%" du3="3000" d3="500" style="position: absolute; width: 445px; height: 300px; top: 100px; left: 600px;">
-                    <img src="../slider/img/new-site/c-phone.png" style="position: absolute; width: 445px; height: 300px; top: 0px; left: 0px;" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                    <img data-u="caption" data-t="CLIP|LR" data-u="4000" t2="NO" src="../slider/img/new-site/c-jssor-slider.png" style="position: absolute; width: 102px; height: 78px; top: 70px; left: 130px;" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                    <img data-u="caption" data-t="ZMF|10" t2="NO" src="../slider/img/new-site/c-text.png" style="position: absolute; width: 80px; height: 53px; top: 153px; left: 163px;" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                    <img data-u="caption" data-t="RTT|10" t2="NO" src="../slider/img/new-site/c-fruit.png" style="position: absolute; width: 140px; height: 90px; top: 60px; left: 220px;" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                    <img data-u="caption" data-t="T" data-u="4000" t2="NO" src="../slider/img/new-site/c-navigator.png" style="position: absolute; width: 200px; height: 155px; top: 57px; left: 121px;" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                </div> -->
-                <div data-u="caption" data-t="RTT|2" data-r="-75%" data-u2="1600" data-d="2500" data-t2="NO" style="position: absolute; width: 470px; height: 220px; top: 120px; left: 650px;">
-                    <img src="img/c-phone-horizontal.png" style="position: absolute; width: 470px; height: 220px; top: 0px; left: 0px;" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                    <img data-u="caption" data-t="MCLIP|L" data-u2="2000" src="img/seattle-drupal-developer-tacoma-pete-lower-web-php-wordpress.jpg" style="position: absolute; width: 379px; height: 213px; top: 4px; left: 45px;" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                    <img data-u="caption" data-t="MCLIP|R" data-u2="2000" data-t2="NO" src="img/seattle-wordpress-developer-tacoma-pete-lower-web-php-drupal.jpg" style="position: absolute; width: 379px; height: 213px; top: 4px; left: 45px;" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                    <img data-u="caption" data-t="RTTL|BR" data-t2="500%" data-y="500%" data-u2="1000" data-d="-3000" data-r="-30%" data-t3="L" data-x3="70%" data-du3="1600" src="img/c-finger-pointing.png" style="position: absolute; width: 257px; height: 300px; top: 80px; left: 200px;" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                    <img src="img/c-navigator-horizontal.png" style="position: absolute; width: 379px; height: 213px; top: 4px; left: 45px;" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
+                <img 
+                    data-u="image" 
+                    src="img/seattle-bridge-web-developer-pete-lower-wordpress-drupal-php.jpg" 
+                    alt="Seattle LAMP Developer and WordPress Expert Pete Lower" 
+                />
+                <div class="caption" data-u="caption" data-t="RTT|2" data-r="-75%" data-u2="1600" data-d="2500" data-t2="NO">
+                    <img 
+                        class="caption-image caption-image-one"
+                        src="img/c-phone-horizontal.png" 
+                        alt="Seattle LAMP Developer and WordPress Expert Pete Lower" 
+                    />
+                    <img 
+                        class="caption-image caption-image-two"
+                        data-u="caption" 
+                        data-t="MCLIP|L" 
+                        data-u2="2000" 
+                        src="img/seattle-drupal-developer-tacoma-pete-lower-web-php-wordpress.jpg"
+                        alt="Seattle LAMP Developer and WordPress Expert Pete Lower" 
+                    />
+                    <img 
+                        class="caption-image caption-image-three"
+                        data-u="caption" 
+                        data-t="MCLIP|R" 
+                        data-u2="2000" 
+                        data-t2="NO" 
+                        src="img/seattle-wordpress-developer-tacoma-pete-lower-web-php-drupal.jpg" 
+                        alt="Seattle LAMP Developer and WordPress Expert Pete Lower" 
+                        />
+                    <img 
+                        class="caption-image caption-image-four"
+                        data-u="caption" 
+                        data-t="RTTL|BR" 
+                        data-t2="500%" 
+                        data-y="500%" 
+                        data-u2="1000" 
+                        data-d="-3000" 
+                        data-r="-30%" 
+                        data-t3="L" 
+                        data-x3="70%" 
+                        data-du3="1600" 
+                        src="img/c-finger-pointing.png" 
+                        alt="Seattle LAMP Developer and WordPress Expert Pete Lower" 
+                    />
+                    <img 
+                        class="caption-image caption-image-five"
+                        src="img/c-navigator-horizontal.png" 
+                        alt="Seattle LAMP Developer and WordPress Expert Pete Lower" 
+                    />
                 </div>
-                <div style="position: absolute; width: 480px; height: 120px; top: 30px; left: 120px; padding: 5px;
-                    text-align: left; line-height: 60px; text-transform: uppercase; font-size: 50px;
-                        color: #FFFFFF;font-family:'Lobster', cursive;">Welcome!
+                <div id="slide-title" class="slide-title">
+                        Welcome!
                 </div>
-                <div style="position: absolute; width: 480px; height: 120px; top: 300px; left: 120px; padding: 5px;
-                    text-align: left; line-height: 36px; font-size: 30px;
-                        color: #FFFFFF;font-family:'Changa One', cursive;">
+                <div id="slide-text" class="slide-text">
                        I'm Pete Lower, you're friendly neighborhood programmer!
                 </div>
             </div>
             <div>
-                <!-- <img data-u="image" src="../slider/img/1920/purple.jpg" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" /> -->
-                <img data-u="image" src="img/seattle-web-developer-tacoma-pete-lower-wordpress-php-drupal.jpg" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                <div style="position: absolute; width: 480px; height: 120px; top: 30px; left: 120px; padding: 5px;
-                    text-align: left; line-height: 60px; text-transform: uppercase; font-size: 50px;
-                        color: #FFFFFF;font-family:'Changa One', cursive;">Emerald City
+                <img 
+                    data-u="image" 
+                    src="img/seattle-web-developer-tacoma-pete-lower-wordpress-php-drupal.jpg" 
+                    alt="Seattle LAMP Developer and WordPress Expert Pete Lower" 
+                />
+                <div id="slide-title" class="slide-title">
+                    Emerald City
                 </div>
-                <div style="position: absolute; width: 480px; height: 120px; top: 300px; left: 120px; padding: 5px;
-                    text-align: left; line-height: 36px; font-size: 30px;
-                        color: #FFFFFF;">
-                        <!-- over slider text -->
+                <div id="slide-text" class="slide-text">
+                    <!-- over slider text -->
                 </div>
             </div>
             <div>
-               <!--  <img data-u="image" src="../slider/img/1920/blue.jpg" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" /> -->
-               <img data-u="image" src="img/in-studio-pete-lower-seattle-musician-rock-band.jpg" alt="Seattle LAMP Developer and WordPress Expert Pete Lower" />
-                <div style="position: absolute; width: 480px; height: 120px; top: 30px; left: 120px; padding: 5px;
-                    text-align: left; line-height: 60px; text-transform: uppercase; font-size: 50px;
-                        color: #FFFFFF;"><!-- over slider text -->
+               <img 
+                    data-u="image" 
+                    src="img/in-studio-pete-lower-seattle-musician-rock-band.jpg" 
+                    alt="Seattle LAMP Developer and WordPress Expert Pete Lower" 
+                />
+                <div id="slide-text" class="slide-text">
+                    <!-- over slider text -->
                 </div>
-                <div style="position: absolute; width: 480px; height: 120px; top: 300px; left: 120px; padding: 5px;
-                    text-align: left; line-height: 36px; font-size: 30px;
-                        color: #FFFFFF;font-family:'Changa One', cursive;">In Studio
-                        <!-- over slider text -->
+                <div id="slide-text" class="slide-text">
+                    In Studio<!-- over slider text -->
                 </div>
             </div>
-            <!-- Example to add fixed static share buttons in slider BEGIN -->
-            <!-- Remove it if no need -->
-            <!-- Share Button Styles -->
-            <!-- Example to add fixed static share buttons in slider BEGIN -->
-            <!-- Remove it if no need -->
-            <!-- Example to add fixed static share buttons in slider END -->
         </div>
-                
-        <!-- Bullet Navigator Skin Begin -->
-        <!-- bullet navigator container -->
-        <div data-u="navigator" class="jssorb21" style="position: absolute; bottom: 26px; left: 6px;">
-            <!-- bullet navigator item prototype -->
-            <div data-u="prototype" style="POSITION: absolute; WIDTH: 19px; HEIGHT: 19px; text-align:center; line-height:19px; color:White; font-size:12px;"></div>
-        </div>
-        <!-- Bullet Navigator Skin End -->
-        <!-- Arrow Navigator Skin Begin -->
+        
+        <!-- <div id="jssorb21" data-u="navigator" class="jssorb21">
+            <div id="prototype" class="prototype" data-u="prototype"></div>
+        </div> -->
         <!-- Arrow Left -->
-        <span data-u="arrowleft" class="jssora21l" style="width: 55px; height: 55px; top: 123px; left: 8px;">
+        <span data-u="arrowleft" class="jssora21l nav-arrow" style="width: 55px; height: 55px; top: 123px; left: 8px;">
         </span>
         <!-- Arrow Right -->
-        <span data-u="arrowright" class="jssora21r" style="width: 55px; height: 55px; top: 123px; right: 8px">
+        <span data-u="arrowright" class="jssora21r nav-arrow" style="width: 55px; height: 55px; top: 123px; right: 8px">
         </span>
-        <!-- Arrow Navigator Skin End -->
-        <a style="display: none" href="http://www.jssor.com">javascript</a>
+
     </div>
     <!-- Jssor Slider End -->
