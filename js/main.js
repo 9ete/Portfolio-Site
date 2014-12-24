dbf9ae8c905591fd6b40b2ec6c6fb05406f23358
hawkeye126 <pete.lower@gmail.com>
2014-12-23 17:34:20 -0800

    add on demand placeholder functions

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ window.onload = function() {
    //document.getElementById("slider1_container").style.visibility = "visible"; //Use visibility to keep a blank placeholder
    document.getElementById("slider1_container").style.display = "block";
 };
+
+$('#soundcloud-player-placeholderimage').click(function(){
+	var video = '<iframe width="'+ $(this).attr('width') +'" height="'+ $(this).attr('height') +'" border="2" src="'+ $(this).attr('data-video') +'"></iframe>';
+	$(this).replaceWith(video);
+});
